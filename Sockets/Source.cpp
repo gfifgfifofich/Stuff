58d21205ca9836666c25f1b57529b46ff8286b05
Gfif <73363091+gfifgfifofich@users.noreply.github.com>
2024-03-04 21:52:21 +0300

    Add files via upload

--- Sockets/Source.cpp
+++ Sockets/Source.cpp
@@ -5,14 +5,14 @@
 #include <chrono>
 #include <thread>
 
-const std::string currentDateTime() {
+std::string currentDateTime() {
 
     // https://stackoverflow.com/questions/997946/how-to-get-current-time-and-date-in-c
-    time_t now = time(0);
+    time_t now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
     struct tm tstruct;
     char buf[80];
     tstruct = *localtime(&now);
-    strftime(buf, sizeof(buf), "%Y-%m-%d.%X", &tstruct);
+    strftime(buf, sizeof(buf), "%Y-%m-%d %T", &tstruct);
 
     return buf;
 }
@@ -45,18 +45,19 @@ int main(int argc, char** argv)
     
     while (1)
     {
-        std::string logtext = '[' + currentDateTime() +"] " ; 
-        logtext += " ";
-        logtext += name;
+      
 
 
         while(1)
         {
+            std::string logtext = '[' + currentDateTime() +"] " ; 
+            logtext += " ";
+            logtext += name;
             if(!cli.Send(logtext.c_str()))
-                std::cout<<"failed ro send initial data to server";
+            std::cout<<"failed ro send initial data to server";
             std::chrono::seconds timespan(d);
             std::this_thread::sleep_for(timespan);
-    }
+        }
 
 
         // std::cin.get() skips one time for some reason
